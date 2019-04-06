dafa8e0d75352190ad49661b571485660effda41
Svetlozar Georgiev <sfe7lio@gmail.com>
2019-04-06 23:46:31 +0100

    implemented basic airlock functionality]

--- Submarine/src/main.adb
+++ Submarine/src/main.adb
@@ -1,6 +1,32 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with submarine; use submarine;
+
 procedure Main is
+   YellowSubmarine : submarine.Submarine;
+   b : AirLock_Idx := YellowSubmarine.airlocks'First;
+   e : AirLock_Idx := YellowSubmarine.airlocks'Last;
 
 begin
-   --  Insert code here.
-   null;
+   Put_Line("Doors: ");
+   for i in b..e loop
+      Put_Line(YellowSubmarine.airlocks(i).door'Image);
+   end loop;
+
+   Put_Line("Locks: ");
+   for i in b..e loop
+      Put_Line(YellowSubmarine.airlocks(i).lock'Image);
+   end loop;
+
+   LockAirLocks(YellowSubmarine.airlocks);
+   CloseAirLocks(YellowSubmarine.airlocks);
+
+   Put_Line("Doors: ");
+   for i in b..e loop
+      Put_Line(YellowSubmarine.airlocks(i).door'Image);
+   end loop;
+
+   Put_Line("Locks: ");
+   for i in b..e loop
+      Put_Line(YellowSubmarine.airlocks(i).lock'Image);
+   end loop;
 end Main;
--- Submarine/src/submarine.ads
+++ Submarine/src/submarine.ads
@@ -1,5 +1,31 @@
-package submarine is
-
+package submarine with SPARK_Mode is
+   
+   -- Types -- 
+   type OpenClosed is (Open, Closed);
+   type LockedUnlocked is (Locked, Unlocked);
+   
+   type AirLock is 
+      record
+         door : OpenClosed;
+         lock : LockedUnlocked;
+      end record;
+   
+   type AirLock_Idx is range 0..1;
+   type SubmarineAirLocks is array (AirLock_Idx) of AirLock;
+      
+   type Submarine is 
+      record 
+         airlocks : SubmarineAirLocks := ( 0 => (Open, Unlocked), 1 => (Open, Unlocked) ); 
+      end record;
+   
+   --     Procedures   
+   procedure CloseAirLocks (s : in out SubmarineAirLocks) with     
+     Pre => (for all I in s'Range => s(I).door = Open),
+     Post => (for all I in s'Range => s(I).door = Closed);
+      
+   procedure LockAirLocks (s : in out SubmarineAirLocks) with     
+     Pre => (for all I in s'Range => s(I).lock = Unlocked),
+     Post => (for all I in s'Range => s(I).lock = Locked);
    
 
 end submarine;
